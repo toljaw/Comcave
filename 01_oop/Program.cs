dd3b5d2809cc14bad198a4e7d2d40d2b441987eb
Anatoli Weikum <weikum.anatoli@gmail.com>
2024-07-08 22:22:39 +0200

    add first test on 01_oop

--- 01_oop/Program.cs
+++ 01_oop/Program.cs
@@ -3,6 +3,9 @@ class Program
 {
     static void Main(string[] args)
     {
+        Console.WriteLine("How old are you? ");
+        string age = Console.ReadLine();
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("I'm " + age + " years old.");
     }
 }
